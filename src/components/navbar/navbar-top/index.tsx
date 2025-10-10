5967016de9c9cbff7f3ea089bb84a235717d585b
Kacper Popczynski <popkacper@gmail.com>
2025-10-10 15:59:09 +0200

    chore: Updated NavbarTop component

--- src/components/navbar/navbar-top/index.tsx
+++ src/components/navbar/navbar-top/index.tsx
@@ -1,34 +1,35 @@
-import NavbarTopStyles from './navbar-top.module.css';
-
 import { OutlinedButton } from '@components/button';
 import { cn } from '@utils/cn';
 import { MenuIcon, TrophyIcon } from 'lucide-react';
-import { NavbarTopLogo } from './navbar-top-logo';
-import { useNavigate } from '@tanstack/react-router';
+import { Link } from '@tanstack/react-router';
 import { PageContainer } from '@components/page-container';
-import { NavbarTopWallet } from './navbar-top-wallet';
+import { Logo } from '@components/logo';
+import { Wallet } from '@features/payments/components/wallet';
 
 type NavbarTopProps = Pick<React.ComponentProps<'div'>, 'className'>;
 
 export const NavbarTop = (props: NavbarTopProps) => {
-    const navigate = useNavigate();
-
     return (
-        <div className={cn('', props.className)}>
-            <PageContainer
-                className={cn(
-                    'flex items-center gap-6 py-4',
-                    NavbarTopStyles['navbar-top--fosted']
-                )}
-            >
-                <NavbarTopLogo />
-                <div className="flex flex-1 items-center gap-4">
-                    <NavbarTopWallet isGlowing={true} />
-                    <div className="bg-primary/15 h-2 w-0.5"></div>
-                    <div className="flex gap-2">
-                        <OutlinedButton icon={TrophyIcon} onClick={() => navigate({ to: '/' })} />
-                        <OutlinedButton icon={MenuIcon} onClick={() => navigate({ to: '/' })} />
-                    </div>
+        <div
+            className={cn(
+                'bg-surface-1/60 border-b brightness-90 saturate-150 backdrop-blur-lg',
+                props.className
+            )}
+        >
+            <PageContainer className="flex items-center justify-between gap-6 py-4">
+                <Link to="/" className="size-10 shrink-0 grow-0 cursor-pointer">
+                    <Logo className="size-full scale-150 object-contain" />
+                </Link>
+                <div className="flex-1">
+                    <Wallet />
+                </div>
+                <div className="flex gap-2">
+                    <Link to="/">
+                        <OutlinedButton icon={TrophyIcon} />
+                    </Link>
+                    <Link to="/">
+                        <OutlinedButton icon={MenuIcon} />
+                    </Link>
                 </div>
             </PageContainer>
         </div>
